47ee11c4e68a1831a5df6f24dfd4d8d658cd732f
Coder Emran <coderemran6910@gmail.com>
2024-01-03 00:10:29 +0600

    page router setup

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,40 @@
 import { createBrowserRouter } from "react-router-dom";
 import ManiLayout from "../layouts/ManiLayout";
+import HomePage from "../pages/HomePage";
+import ContactPage from "../pages/ContactPage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import AboutPage from "../pages/AboutPage";
 
 const routes = createBrowserRouter([
     {
         path: "/",
-        element: <ManiLayout />
-    }
+        element: <ManiLayout />,
+        children: [
+            {
+                index: true,
+                path: "/",
+                element: <HomePage />
+            },
+            {
+                path: "/contact",
+                element: <ContactPage/>
+            },
+            {
+                path: "/about",
+                element: <AboutPage />
+            },
+            {
+                path: "/login",
+                element: <LoginPage />
+            },
+            {
+                path: "/register",
+                element: <RegisterPage />
+            }
+
+        ]
+    },
+    
 ])
 export default routes;
